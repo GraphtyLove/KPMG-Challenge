e81852bdc4278965394ec75ef27d436483b085a5
Oliveri Giuliano <Oliverigiuliano@outlook.com>
2019-09-10 16:45:02 +0200

    Rewrote scraping script
    - Added the meta data scraping to the script
    -Cleaned the code a bit
    -Resolved a bug where the script would scrape the wrong links/meta data
    -added a nice logger

--- Test_files/scrap_pdf.py
+++ Test_files/scrap_pdf.py
@@ -1,6 +1,7 @@
-# %% md
-## Setup + Imports
-# %%
+"""This script scrapes a website and saves pdfs of companies status, sorted by year, as well as corresponding metadata
+path to pdfs: assets/pdf/
+path to meta-data : assets/json/meta-data"""
+
 import urllib, json
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
@@ -10,50 +11,91 @@ import codecs
 import re
 import sys
 
-
 from datetime import datetime
 import time
 import requests
+import datetime
+import numpy as np
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'
 }
-YEAR_LIST = [2014, 2015, 2016, 2017, 2018, 2019]
+YEAR_LIST = [2016, 2017, 2018, 2019]
 
 # get list of url from maxim's github
 for YEAR in YEAR_LIST:
-    print(f'* ____________________________ STARTING YEAR: {YEAR} ____________________________ *')
+    print(f'°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸ STARTING YEAR: {YEAR} °º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸')
     url = f"https://raw.githubusercontent.com/GraphtyLove/KPMG-Challenge/master/assets/json/links_entreprises/links_entreprises_{YEAR}.json"
     response = urlopen(url)
     data = json.loads(response.read())
     data = list(set(data))
     data.sort()
 
-    ## Dowload PDF
-    # %%
+    time_deltas = []
+    saved_time = datetime.datetime.now()
+    # Download PDF
     for i, link in enumerate(data):
+        soup = None
         try:
+            time_deltas.insert(0, datetime.datetime.now() - saved_time)
+            time_deltas = time_deltas[:1000]
+            time_delta = np.mean(time_deltas)
+            saved_time = datetime.datetime.now()
+            print(f'________________ file {i} / {len(data)} ________________ ETA: {time_delta * (len(data) - i)} ________________')
+            print(f'link: {link} ')
             time.sleep(0.1)
             reg_url = link
             req = Request(url=reg_url, headers=headers)
             html = urlopen(req).read()
             soup = BeautifulSoup(html, 'lxml')
-            hrefs = soup.find_all('a', attrs={"target": "_blank"})
-            pdf_url = hrefs[3].attrs['href']
+
+            pdf_url = soup.find('td', text="Oprichtingsakte").findNext('td').findNext('td').findChildren('a')[0].attrs[
+                'href']
+            print('                     pdf_url: ', pdf_url)
 
             monitor_url = 'mopdf'
-            if (monitor_url in pdf_url):
-                print(f'ERROR, suspected monitor from url {i} {pdf_url}')
+            if monitor_url in pdf_url:
+                print(f'               (¬_¬) suspected monitor pdf')
             else:
-                myfile = requests.get(pdf_url)
+                my_file = requests.get(pdf_url)
                 with open(f'../assets/pdf/{YEAR}/pdf-{i}.pdf', 'wb') as file:
-                    file.write(myfile.content)
+                    file.write(my_file.content)
 
-                print(f"PDF n°{i} OK from -> {link}")
-
+                print("               (⚗_⚗) pdf successfully scraped")
         except:
-            print(f'ERROR url: {i} {link}')
-            print(sys.exc_info())
+            print("          (ಠ۾ಠ) Error on scraping the pdf")
+            print('                   ', sys.exc_info())
+
+        # Scrape metadata and save it to json
+
+        try:
+            meta_data = {}
+            title = soup.find_all('td', attrs={"class": "title"}, text=True)
+            m_data = soup.find_all('td', attrs={"colspan": "2"}, text=True)
+            # print('title:')
+            # for j, t in enumerate(title):
+            #     print(j, t.getText())
+            # print('m_data:')
+            # for j, t in enumerate(m_data):
+            #     print(j, t.getText())
+
+            smaller_len = min(len(m_data), len(title))
+            for k in range(smaller_len):
+                value = m_data[k].getText()
+                value_as_string = str(value)
+                if "Laatste" not in value_as_string:
+                    meta_data[title[k].getText()] = value_as_string
+
+            # add the url to the metadata
+            meta_data['url'] = link
+
+            with open(f'../assets/json/meta-data/{YEAR}/{i}.meta.json', 'w', encoding='utf-8') as file:
+                json.dump(meta_data, file, ensure_ascii=False, indent=4)
+
+            print("               (⚗_⚗) meta-data successfully scraped")
+        except:
+            print("                (╯︵╰,) Error on scraping the metadata")
+            print('                   ', sys.exc_info())
 
     # %%
 # ! -------------------- DEBUG -------------------- !
